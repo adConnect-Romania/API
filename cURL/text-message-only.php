69b138fd8b6c66628c2a80dc77fac7a2401ee50b
Laurentiu <127452584+LaurentiuADC@users.noreply.github.com>
2023-05-05 00:18:54 +0300

    Create text-message-only.php

---
+++ cURL/text-message-only.php
@@ -0,0 +1,5 @@
+curl --location --request POST 'https://adconnect.ro/api/create-message' \
+--form 'appkey="<REPLACE-YOUR-APP-KEY>"' \
+--form 'authkey="<REPLACE-YOUR-AUTH-KEY>"' \
+--form 'to="RECEIVER_NUMBER"' \
+--form 'message="Example message"' \
